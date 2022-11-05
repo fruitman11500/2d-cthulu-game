0a8fedd717b52024777f9391b72fa349dec76605
fruitman11500 <114442214+fruitman11500@users.noreply.github.com>
2022-11-05 12:50:22 -0700

    added hero animations

--- Nate/2d cthulu game (real)/Player.gd
+++ Nate/2d cthulu game (real)/Player.gd
@@ -2,20 +2,28 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 var speed = 350
+onready var sprite = $sprite
 
+func _ready():
+	sprite.animation = 'stand_right'
 func get_input():
 	if Input.is_action_pressed("down"):
 		velocity.y = 1
+		sprite.animation = 'down'
 	elif Input.is_action_pressed('up'):
 		velocity.y = -1 
+		sprite.animation = 'up'
 	else:
 		velocity.y = 0
 	if Input.is_action_pressed("left"):
 		velocity.x = -1 
+		sprite.animation = 'left'
 	elif Input.is_action_pressed("right"):
 		velocity.x = 1
+		sprite.animation = 'right'
 	else:
 		velocity.x = 0
+		
 
 func _physics_process(delta):
 	get_input()
