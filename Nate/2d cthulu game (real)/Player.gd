c233cb456f9598f71d21fc460fdb8fa96be3e567
fruitman11500 <114442214+fruitman11500@users.noreply.github.com>
2022-11-09 10:01:45 -0800

    drugs

--- Nate/2d cthulu game (real)/Player.gd
+++ Nate/2d cthulu game (real)/Player.gd
@@ -18,9 +18,13 @@ func get_input():
 	if Input.is_action_pressed("left"):
 		velocity.x = -1 
 		sprite.animation = 'left'
+	elif Input.is_action_just_released("left"):
+		sprite.animation = 'stand_left'
 	elif Input.is_action_pressed("right"):
 		velocity.x = 1
 		sprite.animation = 'right'
+	elif Input.is_action_just_released("right"):
+		sprite.animation = 'stand_right'
 	else:
 		velocity.x = 0
 		
