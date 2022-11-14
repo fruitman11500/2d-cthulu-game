626dedc43a7b3d4395b09b23e3db883f71f99f1a
fruitman11500 <114442214+fruitman11500@users.noreply.github.com>
2022-11-14 13:40:49 -0700

    Did basic movement

--- Eli/Eli game/KinematicBody2D.gd
+++ Eli/Eli game/KinematicBody2D.gd
@@ -1,13 +1,18 @@
 extends KinematicBody2D
 
 var velocity = Vector2()
-
+var speed = 300 
 func get_input():
 	if Input.is_action_pressed("up"):
 		velocity.y =-1
 	if Input.is_action_pressed("down"):
 		velocity.y =1
 	if Input.is_action_pressed("left"):
-		velcity.x =-1
-	if Input.is_action_pressed("right")
-		v
+		velocity.x =-1
+	if Input.is_action_pressed("right"):
+		velocity.x =1
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide(velocity*speed)
+	
